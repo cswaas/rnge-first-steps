02b3665e615ecf0eefed37f621cc1e631da004fa
Christian Waas <cswaas@gmail.com>
2020-02-17 14:46:46 +0100

    Move code frim index.js to App.js

--- index.js
+++ index.js
@@ -1,37 +1,9 @@
-import React, {PureComponent} from 'react';
-import {AppRegistry, StyleSheet, StatusBar} from 'react-native';
-import {GameEngine} from 'react-native-game-engine';
-import {Finger} from './components/renderers';
-import {MoveFinger} from './components/systems';
+/**
+ * @format
+ */
 
-export default class rngeFirstSteps extends PureComponent {
-  constructor() {
-    super();
-  }
+import {AppRegistry} from 'react-native';
+import App from './App';
+import {name as appName} from './app.json';
 
-  render() {
-    return (
-      <GameEngine
-        style={styles.container}
-        systems={[MoveFinger]}
-        entities={{
-          1: {position: [40, 200], renderer: <Finger />}, //-- Notice that each entity has a unique id (required)
-          2: {position: [100, 200], renderer: <Finger />}, //-- and a renderer property (optional). If no renderer
-          3: {position: [160, 200], renderer: <Finger />}, //-- is supplied with the entity - it won't get displayed.
-          4: {position: [220, 200], renderer: <Finger />},
-          5: {position: [280, 200], renderer: <Finger />},
-        }}>
-        <StatusBar hidden={true} />
-      </GameEngine>
-    );
-  }
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#FFF',
-  },
-});
-
-AppRegistry.registerComponent('rngeFirstSteps', () => rngeFirstSteps);
+AppRegistry.registerComponent(appName, () => App);
